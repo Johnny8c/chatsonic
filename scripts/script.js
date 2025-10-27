2c35b54c7f2d0bd0011f7c386de00374c08b5d7c
ErickS <ericksanchez0708@gmail.com>
2025-10-27 16:28:50 -0500

    Estructura de la pagina web

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+// Resaltar item activo
+document.querySelectorAll(".sidebar li").forEach(item => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".sidebar li").forEach(li => li.classList.remove("active"));
+    item.classList.add("active");
+  });
+});
+
+// Toggle modo oscuro
+const toggleBtn = document.getElementById("toggle-theme");
+const icon = toggleBtn.querySelector("i");
+
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode");
+
+  // Cambia el ícono
+  if (document.body.classList.contains("dark-mode")) {
+    icon.classList.replace("fa-moon", "fa-sun");
+  } else {
+    icon.classList.replace("fa-sun", "fa-moon");
+  }
+});
